080982700aad83ca06d75bab6eac20afcd07e4db
yangliOMG <562792542@qq.com>
2018-03-23 15:55:10 +0800

    2

--- sw.js
+++ sw.js
@@ -7,12 +7,31 @@ var cacheList = [
   "ico.jpg"
 ]
 
-// 缓存
+// 注册了 service worker 的页面时，会触发一个叫install事件
 self.addEventListener('install', event=>{
   event.waitUntil(
-    caches.open(cacheStorageKey)
-    .then( cache => cache.addAll(cacheList))
+    caches.open(cacheStorageKey)  //打开缓存仓库
+    .then( cache => cache.addAll(cacheList))  //调用 cache.addAll() 并传入一个 url 列表，然后加载这些资源并将响应添加至缓存
     .then( () => self.skipWaiting())    //在页面更新的过程当中，新的 SW 脚本能够立刻激活和生效。
+  );
+});
+
+// 当 service worker 开始启动时，就会触发 activate 事件。 所以我们监听 activate 在这里更新缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    //获取所有cache名称
+    caches.keys().then( cacheNames => {
+      return Promise.all(
+        //获取所有不同于当前版本名称 cache 下的内容
+        cacheNames.filter( cacheNames => {
+          return cacheNames != cacheStorageKey
+        }).map( cacheNames => {
+          return caches.delete(cacheNames)  //清空缓存池中的无用缓存
+        })
+      );
+    }).then( () => {
+      return self.clients.claim()   ///取得页面的控制权，这样之后打开页面都会使用版本更新的缓存
+    })
   );
 });
 
@@ -26,24 +45,5 @@ self.addEventListener('fetch', event => {
       return fetch(event.request.url)
     })
   )
-});
-
-// 缓存更新
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    //获取所有cache名称
-    caches.keys().then( cacheNames => {
-      return Promise.all(
-        //获取所有不同于当前版本名称 cache 下的内容
-        cacheNames.filter( cacheNames => {
-          return cacheNames != cacheStorageKey
-        }).map( cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      );
-    }).then( () => {
-      return self.clients.claim()   ///取得页面的控制权，这样之后打开页面都会使用版本更新的缓存
-    })
-  );
 });
 
